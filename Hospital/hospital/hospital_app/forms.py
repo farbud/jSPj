6135518e9b89212cd71d7f0a7d46684d36763595
farbod <farbudskate@gmail.com>
2023-11-10 22:33:39 +0330

    up

---
+++ Hospital/hospital/hospital_app/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.contrib.auth.models import User
+from . import models
+
+
+class AdminSigupForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['first_name','last_name','username','password']
+        widgets = {
+        'password': forms.PasswordInput()
+        }
+
+class DoctorUserForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['first_name','last_name','username','password']
+        widgets = {
+        'password': forms.PasswordInput()
+        }
+class DoctorForm(forms.ModelForm):
+    class Meta:
+        model=models.Doctor
+        fields=['address','mobile','department','status']
+
+class PatientUserForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['first_name','last_name','username','password']
+        widgets = {
+        'password': forms.PasswordInput()
+        } 
--- Hospital/hospital/hospital_app/views.py
+++ Hospital/hospital/hospital_app/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,reverse
+from . import forms,models
+from django.db.models import Sum
+from django.contrib.auth.models import Group
+from django.http import HttpResponseRedirect
+from django.core.mail import send_mail
+from django.contrib.auth.decorators import login_required,user_passes_test
+from datetime import datetime,timedelta,date
+from django.conf import settings
+from django.db.models import Q
 
-# Create your views here.
+
+
+def home_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request)
+
+def adminclick_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request)
+
+def doctorclick_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request)
+
+def patientclick_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request)
+
+
+
+def admin_signup_view(request):
+    form=forms.AdminSigupForm()
+    if request.method=='POST':
+        form=forms.AdminSigupForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            user.set_password(user.password)
+            user.save()
+            my_admin_group = Group.objects.get_or_create(name='ADMIN')
+            my_admin_group[0].user_set.add(user)
+            return HttpResponseRedirect('adminlogin')
+        return render(request)
